0c21be875fa3adb38b92e348ffcf1cc1a8d8854c
adandev1125 <adanpopov.dev@gmail.com>
2023-05-21 11:36:06 +0300

    Added template examples in view/welcome.html.

--- controllers/welcome.go
+++ controllers/welcome.go
@@ -5,7 +5,15 @@ import (
 	"net/http"
 )
 
+type WelcomePageData struct {
+	Title         string
+	FirstHtmlFile string
+}
+
 func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("views/welcome.html"))
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, WelcomePageData{
+		Title:         "Go MVC",
+		FirstHtmlFile: "views/welcome.html",
+	})
 }
